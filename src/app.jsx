eaa2dae9934b3ff6581530477702a05e88b05919
Ethan Collier <ethanacollier@icloud.com>
2026-02-14 00:38:54 -0500

    Initial implementation for app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -2,16 +2,37 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
-export default function App() {
-  return <div>
-    <main>App components go here</main>
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
+import { Profile } from './profile/profile';
+import { Practice } from './practice/practice';
+import { Info } from './info/info';
+import { Home } from './home/home';
 
-    <footer className="container-fluid text-center py-3 bg-dark text-white-50">
-        <p className="mb-1"> Created by Ethan Collier </p>
-        <p className="small">
-            View my GitHub repository here:
-            <a href="https://github.com/trobarg/startup">Intuassist on GitHub</a>
-        </p>
-    </footer>
-    </div>;
+export default function App() {
+    <BrowserRouter>
+        return <div>
+            <Routes>
+                <Route path='/' element={<Home />} exact />
+                <Route path='/login' element={<Login />} />
+                <Route path='/signup' element={<Signup />} />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='/practice' element={<Practice />} />
+                <Route path='/info' element={<Info />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+
+            <footer className="container-fluid text-center py-3 bg-dark text-white-50">
+                <p className="mb-1"> Created by Ethan Collier </p>
+                <p className="small">
+                    View my GitHub repository here:
+                    <a href="https://github.com/trobarg/startup">Intuassist on GitHub</a>
+                </p>
+            </footer>
+        </div>;
+    </BrowserRouter >
+}
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }

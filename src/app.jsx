334b82e87f7ad2cab6b2eebaa600b0c7f995f86e
Ethan Collier <ethanacollier@icloud.com>
2026-02-13 23:43:42 -0500

    Created jsx file per page and began modification of app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return <div className="body bg-dark text-light">
+    App will display here
+
+    <footer class="container-fluid text-center py-3 bg-dark text-white-50">
+        <p class="mb-1"> Created by Ethan Collier </p>
+        <p class="small">
+            View my GitHub repository here:
+            <a href="https://github.com/trobarg/startup">Intuassist on GitHub</a>
+        </p>
+    </footer>
+    </div>;
 }

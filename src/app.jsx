a5936494d856cb59ed82003c189aedafaa2014f6
Ethan Collier <ethanacollier@icloud.com>
2026-02-26 21:56:23 -0500

    Wrapped the App return value from app.jsx in my AppProvider tag so it can actually take effect

--- src/app.jsx
+++ src/app.jsx
@@ -9,31 +9,34 @@ import { Profile } from './profile/profile';
 import { Practice } from './practice/practice';
 import { Info } from './info/info';
 import { Home } from './home/home';
+import { AppProvider } from './context/AppContext';
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <div className="d-flex flex-column min-vh-100">
-                <div className="flex-grow-1 d-flex flex-column">
-                    <Routes>
-                        <Route path='/' element={<Home />} exact />
-                        <Route path='/login' element={<Login />} />
-                        <Route path='/signup' element={<Signup />} />
-                        <Route path='/profile' element={<Profile />} />
-                        <Route path='/practice' element={<Practice />} />
-                        <Route path='/info' element={<Info />} />
-                        <Route path='*' element={<NotFound />} />
-                    </Routes>
+        <AppProvider>
+            <BrowserRouter>
+                <div className="d-flex flex-column min-vh-100">
+                    <div className="flex-grow-1 d-flex flex-column">
+                        <Routes>
+                            <Route path='/' element={<Home />} exact />
+                            <Route path='/login' element={<Login />} />
+                            <Route path='/signup' element={<Signup />} />
+                            <Route path='/profile' element={<Profile />} />
+                            <Route path='/practice' element={<Practice />} />
+                            <Route path='/info' element={<Info />} />
+                            <Route path='*' element={<NotFound />} />
+                        </Routes>
+                    </div>
+                    <footer className="container-fluid text-center py-3 bg-dark text-white-50">
+                        <p className="mb-1"> Created by Ethan Collier </p>
+                        <p className="small">
+                            View my GitHub repository here:
+                            <a href="https://github.com/trobarg/startup">Intuassist on GitHub</a>
+                        </p>
+                    </footer>
                 </div>
-                <footer className="container-fluid text-center py-3 bg-dark text-white-50">
-                    <p className="mb-1"> Created by Ethan Collier </p>
-                    <p className="small">
-                        View my GitHub repository here:
-                        <a href="https://github.com/trobarg/startup">Intuassist on GitHub</a>
-                    </p>
-                </footer>
-            </div>
-        </BrowserRouter >
+            </BrowserRouter>
+        </AppProvider>
     );
 }
 function NotFound() {

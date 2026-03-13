7ff5a62c56be34ef185babe585f7c19e1a2901e5
Ethan Collier <ethanacollier@icloud.com>
2026-03-12 20:38:12 -0400

    Added initial index.js for the service, as well as vite.config.js

---
+++ service/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.static('public'));
+
+app.listen(port);
